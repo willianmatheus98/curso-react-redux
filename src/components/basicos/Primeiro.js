a1d2482f546dfb1b79037f3496e0d4f1d0e9ca52
Willian <wmmbwillian@gmail.com>
2020-05-28 15:48:59 -0300

    Primeiro componente

---
+++ src/components/basicos/Primeiro.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Primeiro() {
+    let msg = 'Seja bem vindo(a)!';
+    return (
+        <div>
+            <h1>
+                Primeiro Componente
+            </h1>
+            <p>{msg}</p>
+        </div>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Primeiro from './components/basicos/Primeiro';
+
+import './index.css';
+const root = document.getElementById('root');
+ReactDOM.render(<h1><Primeiro/></h1>, root);
